e850f4cd0903ac5353d72a0e7e8e293897f13a0b
Luca Milanesio <luca.milanesio@gmail.com>
2021-02-23 01:24:32 +0000

    Introduce zookeeper-refdb init step
    During init the plugin needs to check for existing
    Zookeeper-based global-refdb and manage the update
    of the refs modified during an init migration step.
    
    Bug: 14004
    Change-Id: I7836b739bba7b0b84535645c9aa56cc6f9f7d214

---
+++ src/main/java/com/googlesource/gerrit/plugins/validation/dfsrefdb/zookeeper/ZkInit.java
@@ -0,0 +1,51 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.validation.dfsrefdb.zookeeper;
+
+import static com.googlesource.gerrit.plugins.validation.dfsrefdb.zookeeper.ZookeeperConfig.SECTION;
+import static com.googlesource.gerrit.plugins.validation.dfsrefdb.zookeeper.ZookeeperConfig.SUBSECTION;
+
+import com.google.gerrit.pgm.init.api.ConsoleUI;
+import com.google.gerrit.pgm.init.api.InitStep;
+import com.google.gerrit.pgm.init.api.Section;
+import com.google.inject.Inject;
+import org.eclipse.jgit.lib.Config;
+
+public class ZkInit implements InitStep {
+
+  private final ConsoleUI ui;
+  private final Config config;
+
+  @Inject
+  ZkInit(final ConsoleUI ui, final Section.Factory sections) {
+    this.ui = ui;
+
+    this.config = new Config();
+    Section zkSection = sections.get(SECTION, SUBSECTION);
+    for (String key : ZookeeperConfig.KEYS) {
+      String value = zkSection.get(key);
+      if (value != null) {
+        config.setString(SECTION, SUBSECTION, key, value);
+      }
+    }
+  }
+
+  @Override
+  public void run() throws Exception {
+    if (config.getSections().isEmpty()) {
+      ui.message("Zookeeper configuration not found: global-refdb migration skipped");
+    }
+  }
+}
