8afe27d5c66eb6eca1bc76e5f8b8206558a0061a
Daniele Sassoli <danielesassoli@gmail.com>
2024-06-10 19:03:56 +0100

    Add test for zookeeper auth config parsing
    Change-Id: I0695678348e64b7fc7390d4eb5bbcec19f290475

--- src/test/java/com/googlesource/gerrit/plugins/validation/dfsrefdb/zookeeper/ZookeeperTestContainerSupport.java
+++ src/test/java/com/googlesource/gerrit/plugins/validation/dfsrefdb/zookeeper/ZookeeperTestContainerSupport.java
@@ -71,7 +71,7 @@ public class ZookeeperTestContainerSupport {
     when(cfgFactory.getGlobalPluginConfig("zookeeper")).thenReturn(sharedRefDbConfig);
     configuration = new ZookeeperConfig(new Config(), cfgFactory, "zookeeper");
 
-    this.curator = configuration.buildCurator();
+    this.curator = configuration.startCurator();
   }
 
   public void cleanup() {
